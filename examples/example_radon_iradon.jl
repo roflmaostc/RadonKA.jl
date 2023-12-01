79346bfb7b6ea414336489a3b58c35a43fae2d8a
roflmaostc <fxw+git@mailbox.org>
2023-12-01 14:11:18 +0100

    Fix radon with atomicx

--- examples/example_radon_iradon.jl
+++ examples/example_radon_iradon.jl
@@ -37,12 +37,12 @@ md"# Example Image"
 begin
 	N = 320
 	N_z = 100
-	#img = box(Float32, (N, N, N_z), (N ÷4, N ÷ 4, 20), offset=(N ÷ 2 + 60, N ÷ 2 -50, N_z ÷ 2)) |> collect
+	img = box(Float32, (N, N, N_z), (N ÷4, N ÷ 4, 20), offset=(N ÷ 2 + 60, N ÷ 2 -50, N_z ÷ 2)) |> collect
 
 	#img = box(Float32, (N, N, N_z), (1, 1, 1)) |> collect
-	img = box(Float32, (N, N, N_z), (N ÷2, N ÷ 2, 50), offset=(N ÷ 2 - 50, N ÷ 2 + 50, N_z ÷ 2)) |> collect
+	#img = box(Float32, (N, N, N_z), (N ÷2, N ÷ 2, 50), offset=(N ÷ 2 - 50, N ÷ 2 + 50, N_z ÷ 2)) |> collect
 	
-	img .+= 0.0f0 .+ (rr2(Float32, (N, N, N_z)) .< 100 .^2)
+	img .+= 0.0f0 .+ (rr2(Float32, (N, N, N_z), offset=(180, 210, N_z÷2)) .< 30 .^2)
 
 	#img = box(Float32, (100, 100), (3, 3), offset=(51, 51)) |> collect
 end;
@@ -54,7 +54,7 @@ img_c = CuArray(img);
 md"# Radon Transform"
 
 # ╔═╡ b8618268-0892-4abc-ae26-e25e41d07968
-angles = range(0f0, 2f0π, 361)
+angles = range(0f0, 2f0π, 361)[begin:end-1]
 
 # ╔═╡ d2cc6fc6-135b-4c4a-8453-9c5bf9e4a24f
 @time sinogram = radon(img, angles);
@@ -84,81 +84,38 @@ size(sinogram)
 # ╔═╡ edbf1577-0fd4-4261-bd04-499bc1a0debd
 md"# IRadon Transform"
 
-# ╔═╡ 61f17d9e-ed0a-4176-9466-464527c1b10e
-@bind angle_cut1 Slider(1:361, show_value=true)
-
-# ╔═╡ 7bbc33af-7082-42e4-ad5f-1d4273e87fbf
-rad2deg.(Float32.(angles))
-
-# ╔═╡ 7d08ba55-4490-400f-8497-5cbfb3f257c7
-@bind angle_cut2 Slider(angle_cut1:361, show_value=true)
-
-# ╔═╡ ed54c930-4f34-4f3d-9180-514dc59fde15
-@time backproject = iradon(sinogram[:, angle_cut1:angle_cut2, :], angles[angle_cut1:angle_cut2]);
-
-# ╔═╡ 037e9d64-505e-40f9-b710-20f57d29bd17
-# ╠═╡ disabled = true
-#=╠═╡
-#CUDA.@time CUDA.@sync backproject_c = iradon(sinogram_c, CuArray(angles),
-											 backend=CUDABackend());
-  ╠═╡ =#
-
-# ╔═╡ 72d63cbe-67d6-4a9c-80fa-d22743709105
-@bind i_z2 Slider(1:size(sinogram, 3), show_value=true)
-
-# ╔═╡ 365ee0e7-3545-4345-8b0c-8338a59c53b3
-simshow(backproject[:, :, i_z2] .+ 0 .* img[:, :, i_z2])
-
-# ╔═╡ 4c5038bd-d346-4070-822b-bc3e5331d99c
-begin
-	plot(backproject[end, :, i_z2])
-	#plot!(vcat([0.0], sinogram[:, 91, i_z2]))
-	#plot!(vcat([0.0], sinogram[:, 1, i_z2]))
-
-end
-
-# ╔═╡ 3c85e03a-a126-40f7-bcc2-0e928190f757
-vcat([0.0], sinogram[:, 1, i_z2])
-
-# ╔═╡ b06bb885-175c-4cac-8346-f69f7172a9aa
-simshow(Array(backproject_c[:, :, i_z2]))
-
-# ╔═╡ 30f1016a-5b66-48ec-93f5-8c1f1129207e
-Revise.errors()
-
-# ╔═╡ 447b163b-cc06-4427-b734-e0498df35260
-sum(backproject)
-
-# ╔═╡ 267d7684-4a1b-402d-96ae-c3e26b957e0b
-sum(backproject_c)
-
-# ╔═╡ ec342e4e-4ce2-4d26-91f8-4d06a4fad46e
-begin
-	arr = zeros(2,2)
-	arr[1,1] = 1
-	simshow(arr)
-end
-
-# ╔═╡ e24bb409-bd5d-4eca-8cdc-884daece26fa
-
-
 # ╔═╡ 7e27da5a-1b04-4d4c-8c62-eaffa7f4f9ce
-@time backproject2 = RadonKA.iradon2(sinogram, angles);
+@time backproject = RadonKA.iradon(sinogram, angles);
+
+# ╔═╡ 0d2ccb21-a041-4fc4-b8b8-6bdc543e89e6
+rad2deg(angles[90])
+
+# ╔═╡ b9bf49a0-7320-4269-9a6a-ac2533ab5fde
+@bind angle_limit Slider(1:size(sinogram, 2), show_value=true)
 
 # ╔═╡ 93a7ab4a-b2dc-4fc2-bf69-66e6d615103f
-CUDA.@time CUDA.@sync backproject2_cu = RadonKA.iradon2(sinogram_c[:, :, :], CuArray(angles[:]), backend=CUDABackend());
-
-# ╔═╡ a29be556-174a-4ec5-962d-9fdf203d51aa
-
+CUDA.@sync backproject_cu = RadonKA.iradon(sinogram_c[:, begin:angle_limit, :], CuArray(angles[begin:angle_limit]), backend=CUDABackend());
 
 # ╔═╡ 52a86ed8-4504-4d9e-9ea6-6aeaf8540406
 @bind i_z3 Slider(1:size(sinogram, 3), show_value=true)
 
 # ╔═╡ 9d7c41db-adb5-4da2-98ae-96e967c1056e
-simshow(Array(backproject2_cu[:, :, i_z3]))
+simshow(Array(backproject_cu[:, :, i_z3]))
 
-# ╔═╡ c9b84c39-7a74-4893-bbb6-5241a121df04
-Revise.errors()
+# ╔═╡ 81b07387-ede6-4a66-8260-8605cd978ede
+md"# Filtered Backprojection"
+
+# ╔═╡ 32b15077-5e09-4693-8f12-3b2029fe63cc
+CUDA.@time CUDA.@sync filtered_bproj = RadonKA.filtered_backprojection(sinogram_c, CuArray(angles), backend=CUDABackend());
+
+# ╔═╡ bc6e2d40-fcd1-4d7b-8f96-3d4d9e4336de
+@bind i_z4 Slider(1:size(sinogram, 3), show_value=true)
+
+# ╔═╡ eee184e3-8d5d-42fb-81fb-a5d7e59a083a
+simshow(Array(filtered_bproj[:, :, i_z4]))
+
+# ╔═╡ a29be556-174a-4ec5-962d-9fdf203d51aa
+backproject ≈ Array(backproject_cu)
 
 # ╔═╡ Cell order:
 # ╠═4eb3148e-8f8b-11ee-3cfe-854d3bd5cc80
@@ -179,24 +136,14 @@ Revise.errors()
 # ╠═3d584d94-b88f-4738-a470-7db1fb3fb996
 # ╠═375a0179-8592-4d02-9686-d6a85a3eb048
 # ╟─edbf1577-0fd4-4261-bd04-499bc1a0debd
-# ╠═61f17d9e-ed0a-4176-9466-464527c1b10e
-# ╠═7bbc33af-7082-42e4-ad5f-1d4273e87fbf
-# ╠═7d08ba55-4490-400f-8497-5cbfb3f257c7
-# ╠═ed54c930-4f34-4f3d-9180-514dc59fde15
-# ╠═037e9d64-505e-40f9-b710-20f57d29bd17
-# ╠═72d63cbe-67d6-4a9c-80fa-d22743709105
-# ╠═365ee0e7-3545-4345-8b0c-8338a59c53b3
-# ╠═4c5038bd-d346-4070-822b-bc3e5331d99c
-# ╠═3c85e03a-a126-40f7-bcc2-0e928190f757
-# ╠═b06bb885-175c-4cac-8346-f69f7172a9aa
-# ╠═30f1016a-5b66-48ec-93f5-8c1f1129207e
-# ╠═447b163b-cc06-4427-b734-e0498df35260
-# ╠═267d7684-4a1b-402d-96ae-c3e26b957e0b
-# ╠═ec342e4e-4ce2-4d26-91f8-4d06a4fad46e
-# ╠═e24bb409-bd5d-4eca-8cdc-884daece26fa
 # ╠═7e27da5a-1b04-4d4c-8c62-eaffa7f4f9ce
+# ╠═0d2ccb21-a041-4fc4-b8b8-6bdc543e89e6
+# ╠═b9bf49a0-7320-4269-9a6a-ac2533ab5fde
 # ╠═93a7ab4a-b2dc-4fc2-bf69-66e6d615103f
-# ╠═a29be556-174a-4ec5-962d-9fdf203d51aa
-# ╟─52a86ed8-4504-4d9e-9ea6-6aeaf8540406
+# ╠═52a86ed8-4504-4d9e-9ea6-6aeaf8540406
 # ╠═9d7c41db-adb5-4da2-98ae-96e967c1056e
-# ╠═c9b84c39-7a74-4893-bbb6-5241a121df04
+# ╟─81b07387-ede6-4a66-8260-8605cd978ede
+# ╠═32b15077-5e09-4693-8f12-3b2029fe63cc
+# ╠═bc6e2d40-fcd1-4d7b-8f96-3d4d9e4336de
+# ╠═eee184e3-8d5d-42fb-81fb-a5d7e59a083a
+# ╠═a29be556-174a-4ec5-962d-9fdf203d51aa
--- src/iradon.jl
+++ src/iradon.jl
@@ -1,102 +1,19 @@
 export iradon
 
 
-
-
-"""
-    iradon(sinogram, θs; backend=CPU())
-
-`backend` can be either `CPU()` for multithreaded CPU execution or
-`CUDABackend()` for CUDA execution. 
-
-
-"""
-function iradon(sinogram::AbstractArray{T, 3}, angles::AbstractArray{T, 1};
-			    backend=CPU()) where T
-    #@assert isodd(size(sinogram, 1)) && size(sinogram, 2) == length(angles)
-    # this is the actual size we are using for calculation
-	N = size(sinogram, 1)
-	N_angles = size(angles, 1)
-
-    # the only significant allocation
-	img = similar(sinogram, (N + 1, N + 1, size(sinogram, 3)))
-	fill!(img, 0)
-
-    # radius of the cylinder we are projecting through
-	radius = size(img, 1) ÷ 2 - 1
-    # mid point, it is actually N ÷ 2 + 1
-    # but because of how adress the indices, we need 1.5 instead of +1
-    mid = size(img, 1) ÷ 2 + 1#T(1)
-    # the y_dists samplings, in principle we can add this as function parameter 
-	y_dists = similar(img, (size(img, 1) - 1, ))
-	y_dists .= -radius:radius
-
-	#@show typeof(sinogram), typeof(img), typeof(y_dists), typeof(angles)
-	kernel! = iradon_kernel!(backend)
-	kernel!(sinogram::AbstractArray{T}, img, y_dists, angles, mid, radius,
-					ndrange=(N, N, size(img, 3)))
-	
-	return img 
-end
-
-"""
-    iradon_kernel!(sinogram, img,
-                  y_dists, angles, mid, radius)
-
-"""
-@kernel function iradon_kernel!(sinogram::AbstractArray{T},
-			img, y_dists, angles, mid, radius) where T
-    # r is the index of the angles
-    # k is the index of the detector spatial coordinate
-    # i_z is the index for the z dimension
-	i_i, i_j, i_z = @index(Global, NTuple)
-
-    if (i_i - mid)^2 + (i_j - mid)^2 > radius^2
-    
-    else
-	    l = 1
-        # acculumators of the intensity
-	    tmp = zero(T)
-        
-        for i_angle in 1:length(angles)
-            angle = angles[i_angle]
-#            if 0 ≤ angle ≤ π / 2 
-                # we need to find out which ray potentially intersects those positions
-                contact_x = sin(angle)
-                contact_y = -cos(angle)
-                dir_x = cos(angle)
-                dir_y = sin(angle)
-                if 0 ≤ angle < π
-                    y_intersect = mid + floor(Int, (dir_x * (i_i - mid - contact_x * radius) + dir_y * (i_j - mid - contact_y * radius)))
-                elseif π <= angle
-                    y_intersect = mid + floor(Int, (dir_x * (i_i - mid - contact_x * radius) + dir_y * (i_j - mid - contact_y * radius)))
-                end
-
-                #println(angle, " ", y_intersect)
-
-            tmp += sinogram[y_intersect, i_angle, i_z] 
-	    end
-        img[i_j,i_i, i_z] = tmp
-    end
-end
-
-
-
-
-
 function filtered_backprojection(sinogram::AbstractArray{T, 3}, θs, μ=nothing; backend=CPU()) where T
     filter = similar(sinogram, (size(sinogram, 1),))
     filter .= rr(T, (size(sinogram, 1), )) 
 
     p = plan_fft(sinogram, (1,))
     sinogram = real(inv(p) * (p * sinogram .* ifftshift(filter)))
-    return iradon(sinogram, θs, μ, backend=backend)
+    return iradon(sinogram, θs, backend=backend)
 end
 
 
 
 
-function iradon2(sinogram::AbstractArray{T, 3}, angles::AbstractArray{T, 1};
+function iradon(sinogram::AbstractArray{T, 3}, angles::AbstractArray{T, 1};
 			    backend=CPU()) where T
     #@assert isodd(size(sinogram, 1)) && size(sinogram, 2) == length(angles)
     # this is the actual size we are using for calculation
@@ -117,7 +34,7 @@ function iradon2(sinogram::AbstractArray{T, 3}, angles::AbstractArray{T, 1};
 	y_dists .= -radius:radius
 
 	#@show typeof(sinogram), typeof(img), typeof(y_dists), typeof(angles)
-	kernel! = iradon_kernel2!(backend)
+	kernel! = iradon_kernel!(backend)
 	kernel!(sinogram::AbstractArray{T}, img, y_dists, angles, mid, radius,
 					ndrange=(N_angles, N, size(img, 3)))
 
@@ -129,7 +46,7 @@ end
                   y_dists, angles, mid, radius)
 
 """
-@kernel function iradon_kernel2!(sinogram::AbstractArray{T},
+@kernel function iradon_kernel!(sinogram::AbstractArray{T},
 			img, y_dists, angles, mid, radius) where T
     # r is the index of the angles
     # k is the index of the detector spatial coordinate
